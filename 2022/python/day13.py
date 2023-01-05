2b15589247cb087a87ff5fd4fb0373d66c160879
amirhosseinzibaei <amirhosseinzibaei@gmail.com>
2023-01-05 20:12:59 +0330

    solve part two day 13

--- 2022/python/day13.py
+++ 2022/python/day13.py
@@ -1,27 +1,31 @@
 from read_input import get_input_data
-
+from functools import cmp_to_key
 
 input_data = get_input_data(day=13)
 
 
 def solve_puzzle_one() -> int:
     pairs = input_data.split('\n\n')
-
     sum_ = 0
     for pair_id in range(1, len(pairs)+1):
         left, right = [eval(p) for p in pairs[pair_id-1].splitlines()]
         status = compare(left, right)
-        if status == 1:
+        if status == -1:
             sum_ += pair_id
     return sum_
 
 
+def solve_puzzle_two() -> int:
+    flatten_paris = input_data.replace('\n\n', '\n')
+    pairs = [[[2]], [[6]]] + [eval(s) for s in flatten_paris.splitlines()]
+    pairs.sort(key=cmp_to_key(compare))
+    return (pairs.index([[2]])+1) * (pairs.index([[6]])+1) 
+
 def compare(left: list | int, right: list | int) -> int:
     '''
     returns:
-        1: Order
-        0: Not Order
-        -1: Equal but left side ran
+        -1: Order
+        1: Not Order
     '''
     if isinstance(left, list) and isinstance(right, list):
         ran = False
@@ -37,7 +41,7 @@ def compare(left: list | int, right: list | int) -> int:
                 return res
 
         if ran:
-            return 1 if len(left) < len(right) else -1
+            return -1 if len(left) < len(right) else 1
 
     if isinstance(left, int) and isinstance(right, list):
         return compare([left], right)
@@ -46,11 +50,12 @@ def compare(left: list | int, right: list | int) -> int:
     
     if isinstance(left, int) and isinstance(right, int):
         if int(left) > int(right):
-            return 0
-        elif int(left) < int(right):
             return 1
+        elif int(left) < int(right):
+            return -1
 
 
 if __name__ == '__main__':
     print('Part One: ', solve_puzzle_one())
+    print('Part Two: ', solve_puzzle_two())
     
