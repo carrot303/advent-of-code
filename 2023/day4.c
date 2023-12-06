5687ec534f79452766699a1d2e3e2d0ffa01705c
amirhosseinzibaei <amirhosseinzibaei@gmail.com>
2023-12-06 18:59:20 +0330

    2023:4 improve python and written in c

---
+++ 2023/day4.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+#define MAX_LINE 200
+
+
+int power(int i, int b) {
+	int r = 1;
+	while (b-- > 0) {
+		r *= i;
+	}
+	return r;
+}
+
+int main() {
+	FILE *fp;
+	char line[MAX_LINE];
+	int winnumbers[10];
+	int own_numbers[25];
+	int scrathcards[250] = {0};
+	int card_id = 0;
+	int number = 0;
+	int result = 0;
+
+	fp = fopen("inputs/day4.txt", "r");
+	if (fp == NULL) {
+		perror("Error in opening file day4.txt");
+		return 1;
+	}
+
+	while ((fgets(line, MAX_LINE, fp)) != NULL) {
+		int k = 0;
+		for (int side=0, i=0; line[i] != '\0'; i++) {
+			if (line[i] >= '0' && line[i] <= '9') {
+				number = (line[i] - '0') + number * ((number!=0)?10:1);
+			} else if (line[i] == ':') {
+				card_id = number;
+				number = 0;
+			} else if (line[i] == ' ' && number > 0) {
+				if (side == 0) {
+					winnumbers[k++] = number;
+					number = 0;
+				} else if (side == 1) {
+					own_numbers[k++] = number;
+					number = 0;
+				}
+			} else if (line[i] == '|') {
+				side = 1;
+				k = 0;
+			}
+		}
+		if (number > 0) {
+			own_numbers[k] = number;
+			number =0 ;
+		}
+		
+		// Part one
+		int same_cards = 0;
+		for (int i = 0; i < 25; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (own_numbers[i] == winnumbers[j]) {
+					same_cards++;
+				}
+			}
+		}
+		scrathcards[card_id]++;
+		if (same_cards) {
+			result += power(2, same_cards-1);
+		}
+		for (int i = 0; i < scrathcards[card_id]; i++) {
+			for (int j = card_id+1; j < card_id+same_cards+1; j++) {
+				scrathcards[j]++;
+			}
+		}
+	}
+	printf("Part one: %d\n", result);
+
+	result = 0;
+	for (int i=0; i < 250; i++) {
+		if (scrathcards[i] > 0) {
+			result += scrathcards[i];
+		}
+	}
+	printf("Part two: %d\n", result);
+
+	fclose(fp);
+	return 0;
+}
--- 2023/day4.py
+++ 2023/day4.py
@@ -4,17 +4,17 @@ with open("inputs/day4.txt") as file:
 	puzzle = file.read().strip()
 
 
-scrathcards = Counter()
+scrathcards = [0 for _ in range(250)]
 result = 0
 for i, card in enumerate(puzzle.split("\n"), 1):
-	scrathcards.update({i:1})
+	scrathcards[i] += 1
 	winning_numbers, numbers = [{int(num) for num in part.split()} for part in card.split(':')[1].split("|")]
 	wnumbers = numbers.intersection(winning_numbers)
 	if wnumbers:
 		result += 2**(len(wnumbers)-1)
 	for _ in range(scrathcards[i]):
 		for j in range(len(wnumbers)):
-			scrathcards.update({j+i+1:1})
+			scrathcards[j+i+1] += 1
 
 print("Part one:", result)
-print("Part two:", scrathcards.total())
+print("Part two:", sum(scrathcards))
