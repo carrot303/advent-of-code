232ede0d9801a5030ffd24bc53224acab9ea7105
amirhosseinzibaei <amirhosseinzibaei@gmail.com>
2023-12-08 02:15:38 +0330

    part two is wrong

--- 2023/day5.py
+++ 2023/day5.py
@@ -9,10 +9,6 @@ first, *maps = puzzle.split("\n\n")
 # Part one
 seeds = list(map(int, first.split()[1:]))
 
-# Part two
-seed_starts = list(map(int,first.split()[1::2]))
-seed_length = list(map(int,first.split()[2::2]))
-
 corresponds = defaultdict(list)
 
 
@@ -24,13 +20,11 @@ def get_correspond(std, number):
 			return (number-source)+dest
 	return number
 
-
 for m in maps:
 	a = m.split("\n")
 	source_to_dest = a[0].split()[0]
 	for i in range(1, len(a)):
 		corresponds[source_to_dest].append(list(map(int,a[i].split())))
-
 
 # Part one
 lowest_location = 1e10
@@ -43,21 +37,5 @@ for seed in seeds:
 	humidity = get_correspond("temperature-to-humidity", temperature)
 	location = get_correspond("humidity-to-location", humidity)
 	lowest_location = location if location < lowest_location else lowest_location
-
-print(lowest_location)
-
-
-# Part two
-lowest_location = 1e10
-for r in range(len(seed_starts)):
-	for seed in range(seed_starts[r],seed_starts[r]+seed_length[r]):
-		soil = get_correspond("seed-to-soil", seed)
-		fertilizer = get_correspond("soil-to-fertilizer", soil)
-		water = get_correspond("fertilizer-to-water", fertilizer)
-		light = get_correspond("water-to-light", water)
-		temperature = get_correspond("light-to-temperature", light)
-		humidity = get_correspond("temperature-to-humidity", temperature)
-		location = get_correspond("humidity-to-location", humidity)
-		lowest_location = location if location < lowest_location else lowest_location
 
 print(lowest_location)
