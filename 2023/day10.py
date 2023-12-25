afe6add51d6b64621ae0c1a061481c25b1267bcd
amirhosseinzibaei <amirhosseinzibaei@gmail.com>
2023-12-25 13:26:27 +0330

    2023:10:2.py

--- 2023/day10.py
+++ 2023/day10.py
@@ -106,3 +106,71 @@ while res1 or res2:
 		break
 
 print("Part one:", step)
+
+
+# Part two (ray-casting-algorithm)
+import sys
+
+_huge = sys.float_info.max
+_eps = 0.00001
+
+def edges(points):
+	e = []
+	for i in range(len(points)):
+		p1 = points[i]
+		p2 = points[(i+1) % len(points)]
+		e.append((p1,p2))
+	return e
+
+
+def is_inside(p):
+	result = False
+	p = [p[0],p[1]]
+	for edge in edges_list:
+		a,b = edge
+		if a[1] > b[1]:
+			a,b = b,a
+		if p[1] == a[1] or p[1] == b[1]:
+			p[1] += _eps
+
+		if (p[1] > b[1] or p[1] < a[1] or p[0] > max(a[0],b[0])):
+			continue
+		if p[0] < min(a[0], b[0]):
+			result = not result
+			continue
+
+		try:
+			m_edge = (b[1]-a[1])/(b[0]-a[0])
+		except ZeroDivisionError:
+			m_edge = _huge
+
+		try:
+			m_point = (p[1]-a[1])/(p[0]-a[0])
+		except ZeroDivisionError:
+			m_point = _huge
+
+		if m_point >= m_edge:
+			result = not result
+			continue
+	return result
+
+
+# Make our polygon
+i, j, enter = *start_position, ENTER_DIRECTIONS[S_band][0]
+res = True
+points = []
+new_board = [["." for _ in range(len(board[0]))] for _ in range(len(board))]
+while res:
+	new_board[i][j] = "1"
+	if board[i][j] in ["7","F","L","J"]:
+		points.append((i,j))
+	res, i, j, enter = go_away(i, j, enter)
+
+edges_list = edges(points)
+insides = 0
+for i in range(len(new_board)):
+	for j in range(len(new_board[0])):
+		if new_board[i][j] == "." and is_inside((i,j)): # it's a point
+			insides += 1
+
+print("Part two:", insides)
